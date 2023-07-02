1d8e27ac6f59810ac46e48cf7e6064c5722c92bc
Hiep <tcmytt@gmail.com>
2023-07-02 21:39:07 +0700

    first

---
+++ test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from time import sleep # nhập hàm sleep từ thư viện time
+def nonsen():
+   s= "howkteam"
+   lst = [s, (1, 2, 3), {'abc', 'xyz'}]
+   for i in lst:
+      print(i)
+if __name__ == "__main__" :
+   print("123")
+   nonsen()
